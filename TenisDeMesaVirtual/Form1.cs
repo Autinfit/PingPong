3b44941154552affc2077c6fd999e1e29d951a6e
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2024-02-21 20:21:37 -0300

    Se agregaron nuevos eventos más las imágenes del archivo.

--- TenisDeMesaVirtual/Form1.cs
+++ TenisDeMesaVirtual/Form1.cs
@@ -6,5 +6,38 @@ namespace TenisDeMesaVirtual
         {
             InitializeComponent();
         }
+
+        private void EventoTemporizadorJuego(object sender, EventArgs e)
+        {
+            // EVENTO EN DONDE FRECUENTEMENTE EL JUEGO CONSTA DE UN TEMPORIZADOR POR DEFECTO.
+
+            // EN INSTANTES...
+        }
+
+        private void EventoPresionarTeclas(object sender, KeyEventArgs e)
+        {
+            // EVENTO EN DONDE EL JUGADOR PRESIONA CUALQUIER TECLA DEL JUEGO.
+
+            // EN INSTANTES...
+        }
+
+        private void EventoSoltarTeclas(object sender, KeyEventArgs e)
+        {
+            // EVENTO EN DONDE EL JUGADOR SUELTA CUALQUIER TECLA DEL JUEGO.
+
+            // EN INSTANTES...
+        }
+
+        // CREAREMOS EVENTOS ADICIONALES PARA QQUE EL JUEGO CHEQUEE LAS COLISIONES Y PARA CUANDO FINALIZA LA PARTIDA.
+
+        private void ChequearColisiones(PictureBox imagen1, PictureBox imagen2, int offSet) // RECIBEN POR PARÁMETROS LAS PRIMERAS 2 IMÁGENES Y UNO FUERA DEL ÁREA DEL JUEGO.
+        {
+            // EN INSTANTES...
+        }
+
+        private void FinDelJuego(string mensaje) // RECIBE POR PARÁMETRO UN MENSAJE FINAL AL FINALIZAR LA PARTIDA.
+        {
+            // EN INSTANTES...
+        }
     }
 }
